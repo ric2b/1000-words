4055dd1fca31183ef06ac15254aee1ed2e48efe6
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-02 02:37:04 +0100

    setup static adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,10 @@
+const adapter_static = require('@sveltejs/adapter-static');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   kit: {
+    adapter: adapter_static(),
+
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
   },
