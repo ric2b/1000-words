6fdd1570075ea982839e4290d1bc6a19a1b0454c
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-02 02:04:29 +0100

    basic functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let language = 'Deutsch';
+
+    let phrases = {
+        'Wichtig': 'Important',
+        'Gut Verdienen': 'Ganhar bem',
+        'Groß': 'Big',
+    };
+
+    let phraseIterator = Object.entries(phrases)[Symbol.iterator]();
+    let next, translationRevealed;
+
+    $: current = phraseIterator.next();
+
+    $: {
+        current; // block is triggered when current is updated
+        translationRevealed = false;
+        next = phraseIterator.next();
+    }
+
+    $: [currentPhrase, currentTranslation] = current.value;
+    $: finished = next.done;
+
+    const revealTranslation = () => translationRevealed = true;
+    const getNextPhrase = () => current = next;
+    const resetIterator = () => phraseIterator = Object.entries(phrases)[Symbol.iterator]();
+
+    function shuffle() {
+        let shuffled = Object.entries(phrases)
+            .map((a) => ({sort: Math.random(), value: a}))
+            .sort((a, b) => a.sort - b.sort)
+            .map((a) => a.value);
+        phraseIterator = shuffled[Symbol.iterator]();
+    }
+    
+</script>
+
+<h1>{language} 1000 most common words</h1>
+
+<p>{currentPhrase}</p>
+
+{#if !translationRevealed}
+    <button on:click={revealTranslation}>Reveal translation</button>
+{:else}
+    <p>{currentTranslation}</p>
+{/if}
+
+<br>
+
+{#if !finished}
+    <button on:click={getNextPhrase}>Next</button>
+{:else}
+    <button on:click={resetIterator}>Reset</button>
+{/if}
+
+<button on:click={shuffle}>Shuffle</button>
