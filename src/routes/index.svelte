fc2c2b64e74c1cdc0ea6aee8bf997ee42e2cb1c1
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-02 17:19:44 +0100

    use csv file as flipcards source

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,22 @@
+<script context="module">
+  export async function load({ page, fetch, session, context }) {
+    const url = 'de-en.csv'; // https://1000mostcommonwords.com/1000-most-common-german-words/ + https://www.convertjson.com/html-table-to-json.htm
+    const response = await fetch(url);
+
+    if (response.ok) {
+      return { props: { phrases: await response.text().then(text => text.split('\n').map(x => x.split(','))) } };
+    }
+
+    return { status: response.status, error: new Error(`Could not load ${url}`) };
+  }
+</script>
+
 <script>
+  export let phrases;
+
   let language = 'Deutsch';
 
-  let phrases = {
-    Wichtig: 'Important',
-    'Gut Verdienen': 'Ganhar bem',
-    Groß: 'Big',
-  };
-
-  let phraseIterator = Object.entries(phrases)[Symbol.iterator]();
+  let phraseIterator = phrases[Symbol.iterator]();
   let next, translationRevealed;
 
   $: current = phraseIterator.next();
@@ -26,7 +35,7 @@
   const resetIterator = () => (phraseIterator = Object.entries(phrases)[Symbol.iterator]());
 
   function shuffle() {
-    let shuffled = Object.entries(phrases)
+    let shuffled = phrases
       .map((a) => ({ sort: Math.random(), value: a }))
       .sort((a, b) => a.sort - b.sort)
       .map((a) => a.value);
