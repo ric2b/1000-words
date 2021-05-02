a084980907ef7fb89ee7da82edaad31fbbf8c361
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-02 02:10:44 +0100

    change prettier format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,37 @@
 <script>
-    let language = 'Deutsch';
+  let language = 'Deutsch';
 
-    let phrases = {
-        'Wichtig': 'Important',
-        'Gut Verdienen': 'Ganhar bem',
-        'Groß': 'Big',
-    };
+  let phrases = {
+    Wichtig: 'Important',
+    'Gut Verdienen': 'Ganhar bem',
+    Groß: 'Big',
+  };
 
-    let phraseIterator = Object.entries(phrases)[Symbol.iterator]();
-    let next, translationRevealed;
+  let phraseIterator = Object.entries(phrases)[Symbol.iterator]();
+  let next, translationRevealed;
 
-    $: current = phraseIterator.next();
+  $: current = phraseIterator.next();
 
-    $: {
-        current; // block is triggered when current is updated
-        translationRevealed = false;
-        next = phraseIterator.next();
-    }
+  $: {
+    current; // block is triggered when current is updated
+    translationRevealed = false;
+    next = phraseIterator.next();
+  }
 
-    $: [currentPhrase, currentTranslation] = current.value;
-    $: finished = next.done;
+  $: [currentPhrase, currentTranslation] = current.value;
+  $: finished = next.done;
 
-    const revealTranslation = () => translationRevealed = true;
-    const getNextPhrase = () => current = next;
-    const resetIterator = () => phraseIterator = Object.entries(phrases)[Symbol.iterator]();
+  const revealTranslation = () => (translationRevealed = true);
+  const getNextPhrase = () => (current = next);
+  const resetIterator = () => (phraseIterator = Object.entries(phrases)[Symbol.iterator]());
 
-    function shuffle() {
-        let shuffled = Object.entries(phrases)
-            .map((a) => ({sort: Math.random(), value: a}))
-            .sort((a, b) => a.sort - b.sort)
-            .map((a) => a.value);
-        phraseIterator = shuffled[Symbol.iterator]();
-    }
-    
+  function shuffle() {
+    let shuffled = Object.entries(phrases)
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value);
+    phraseIterator = shuffled[Symbol.iterator]();
+  }
 </script>
 
 <h1>{language} 1000 most common words</h1>
@@ -40,17 +39,17 @@
 <p>{currentPhrase}</p>
 
 {#if !translationRevealed}
-    <button on:click={revealTranslation}>Reveal translation</button>
+  <button on:click={revealTranslation}>Reveal translation</button>
 {:else}
-    <p>{currentTranslation}</p>
+  <p>{currentTranslation}</p>
 {/if}
 
-<br>
+<br />
 
 {#if !finished}
-    <button on:click={getNextPhrase}>Next</button>
+  <button on:click={getNextPhrase}>Next</button>
 {:else}
-    <button on:click={resetIterator}>Reset</button>
+  <button on:click={resetIterator}>Reset</button>
 {/if}
 
 <button on:click={shuffle}>Shuffle</button>
