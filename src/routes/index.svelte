f6969a19e471b2c10eecbbd977da6d3a87605aaf
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-07 00:54:50 +0100

    fix missing bare.css file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -118,7 +118,7 @@
   </Actions>
 </Card>
 
-<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<link rel="stylesheet" href="bare.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
