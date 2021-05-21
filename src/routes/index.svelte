3c29b724661965041329d06041c3c869bf800e78
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-21 17:15:45 +0100

    Load list information from a file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <script context="module">
+  import { base, assets } from '$app/paths';
+
   export async function load({ page, fetch, session, context }) {
-    const defaultList = 'de-en';
-    return { status: 307, redirect: `/list/${defaultList}` }
+    const listMetadata = await (await fetch(`${assets}/lists/metadata.json`)).json();
+    return { status: 307, redirect: `${base}/list/${listMetadata.defaultList}` }
   }
 </script>
