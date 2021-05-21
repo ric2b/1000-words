853a9c9bf6cdfce1c4a517cb41dc03dd8c0d9a38
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-05-21 16:00:59 +0100

    test7

--- src/routes/list/[slug].svelte
+++ src/routes/list/[slug].svelte
@@ -70,8 +70,8 @@
 
 <h1 on:mouseenter={() => header = 'The 1000 most common german words'} on:mouseleave={() => header = 'Die 1000 häufigsten deutschen Wörter'}>{header}</h1>
 
-<a hidden href="/list/de-en">1</a>
-<a hidden href="/list/verben-mit-praepositionen">2</a>
+<a hidden href="/list/de-en"/>
+<a hidden href="/list/verben-mit-praepositionen"/>
 
 <Card>
   {#key phrases.length} <!-- Workaround for https://github.com/hperrin/svelte-material-ui/issues/247 -->
