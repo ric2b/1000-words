f8cff2360dc7596b308c42736e8a85c141ed20fe
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2023-02-13 02:20:26 +0000

    Improve test config

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,11 @@ const config = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	reporter: [['list'], ['html']],
+	use: { trace: 'retain-on-failure' },
+    // testMatch: '**/*e2e.js',
+	testDir: 'tests',
+	timeout: 2000
 };
 
 export default config;
